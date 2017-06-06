cbfbc73cfcfade3259eacc63b71bc6a1a1bbe496
liujohnson118@yahoo.com <geyi@Geyis-MacBook-Pro.local>
2017-06-06 16:56:04 -0700

    url shortening part 2 done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
+const randomStringLength=6;
 app.use(bodyParser.urlencoded({extended: true}));
 
 // set the view engine to ejs
@@ -13,8 +14,20 @@ app.set('view engine', 'ejs');
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "lexus1": "http://www.lexus.ca",
 };
+
+function generateRandomString(length){
+  var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < length; i++ ){
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -28,16 +41,34 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL=req.params.shortURL.substring(1);
+  const longURL=urlDatabase[shortURL];
+  res.redirect(longURL);
+ });
+
 // about page
 app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("pages/urls_index", templateVars);
+app.get("/urls/new", (req, res) => {
+  res.render("pages/urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  //console.log(req.body.longURL);  // debug statement to see POST parameters
+  const randomString = generateRandomString(randomStringLength);
+  urlDatabase[randomString]=req.body.longURL;
+  res.send(randomString);         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id.substring(1), longURL: urlDatabase[req.params.id.substring(1)] };
+  res.render("pages/urls_show", templateVars);
 });
 
 app.listen(8080);
 console.log('8080 is the magic port');
+
 
